0b63185cee4b1966507f92851aa508e900bf2d5c
hanifjamadar77 <jamadarmhanif@gmail.com>
2025-12-17 17:29:04 +0530

    Error Handling

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ import userRouter from "./Routes/user.route.js";
 import subscriptionRouter from "./Routes/subscription.route.js";
 import authRouter from "./Routes/auth.routes.js";
 import connectToDatabase from "./database/mangodb.js";
+import errorMiddleware from "./middelwares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
 
 app.get("/",(req,res) =>{
     res.send("Welcome to the backend service");
