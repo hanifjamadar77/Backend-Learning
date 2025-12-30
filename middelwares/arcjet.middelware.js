2e95ffc810fec87fbcb15a195ff247a6f4f0c659
hanifjamadar77 <jamadarmhanif@gmail.com>
2025-12-30 23:53:16 +0530

    arcjet updated

--- middelwares/arcjet.middelware.js
+++ middelwares/arcjet.middelware.js
@@ -2,7 +2,7 @@ import aj from "../config/arcjet.js";
 
 const arcjetMiddleware = async (req, res, next) => {
     try {
-        const decision = await aj.protect(req, {requested : 10});
+        const decision = await aj.protect(req, {requested : 1});
         
         if(decision.isDenied()){
             if(decision.reason.isRateLimit()){
@@ -10,13 +10,12 @@ const arcjetMiddleware = async (req, res, next) => {
                     message : "Too many requests. Please try again later."
                 });
             }
-        }
-
-        if(decision.reason.isBot()){
+             if(decision.reason.isBot()){
             return res.status(403).json({
                 message : "Access denied. Bot traffic is not allowed."
             });
         }
+    }
         next();
     }catch (error) {
         console.log("Arcjet Middleware Error:", error);
